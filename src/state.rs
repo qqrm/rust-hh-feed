34802e4bfb02a9ce2c1e13395ee2d8bf59fa2ed0
Alexey <4681325+qqrm@users.noreply.github.com>
2025-07-08 20:56:37 +0300

    Add basic implementation and lock Rust version

---
+++ src/state.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use std::{collections::HashMap, fs, path::Path};
+
+pub type PostedJobs = HashMap<String, String>;
+
+pub fn load_posted_jobs(path: &Path) -> Result<PostedJobs> {
+    if path.exists() {
+        let data = fs::read_to_string(path)?;
+        let map = serde_json::from_str(&data)?;
+        Ok(map)
+    } else {
+        Ok(HashMap::new())
+    }
+}
+
+pub fn save_posted_jobs(path: &Path, map: &PostedJobs) -> Result<()> {
+    let data = serde_json::to_string_pretty(map)?;
+    fs::write(path, data)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    #[tokio::test]
+    async fn load_and_save_cycle() {
+        let path = PathBuf::from("test_posted_jobs.json");
+        let mut map = PostedJobs::new();
+        map.insert("1".into(), "2024-07-08".into());
+        save_posted_jobs(&path, &map).unwrap();
+        let loaded = load_posted_jobs(&path).unwrap();
+        assert_eq!(map, loaded);
+        std::fs::remove_file(&path).unwrap();
+    }
+}
---
+++ src/telegram.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use reqwest::Client;
+use serde::Serialize;
+
+pub struct TelegramBot {
+    token: String,
+    chat_id: String,
+    client: Client,
+}
+
+#[derive(Serialize)]
+struct Message<'a> {
+    chat_id: &'a str,
+    text: &'a str,
+    parse_mode: &'a str,
+}
+
+impl TelegramBot {
+    pub fn new(token: String, chat_id: String) -> Self {
+        Self {
+            token,
+            chat_id,
+            client: Client::new(),
+        }
+    }
+
+    pub async fn send_message(&self, text: &str) -> Result<()> {
+        let msg = Message {
+            chat_id: &self.chat_id,
+            text,
+            parse_mode: "Markdown",
+        };
+        self.client
+            .post(format!(
+                "https://api.telegram.org/bot{}/sendMessage",
+                self.token
+            ))
+            .json(&msg)
+            .send()
+            .await?;
+        Ok(())
+    }
+}
